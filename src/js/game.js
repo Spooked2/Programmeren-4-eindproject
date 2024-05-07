c15b4c938308cf498ee947d481b1a768479cd4cf
Sander <sanderlandm@gmail.com>
2024-05-07 16:59:59 +0200

    Added assets

--- src/js/game.js
+++ src/js/game.js
@@ -10,12 +10,25 @@ export class Game extends Engine {
     }
 
     startGame() {
-        console.log("start de game!")
-        const fish = new Actor()
-        fish.graphics.use(Resources.Fish.toSprite())
-        fish.pos = new Vector(400, 300)
-        fish.vel = new Vector(-10,0)
-        this.add(fish)
+        console.log("start de game!");
+        const fish = new Actor();
+        fish.graphics.use(Resources.Fish.toSprite());
+        fish.pos = new Vector(400, 300);
+        fish.vel = new Vector(-10,0);
+        this.add(fish);
+
+        const knight = new Actor();
+        knight.graphics.use(Resources.Knight.toSprite());
+        knight.pos = new Vector(50, 300);
+        knight.vel = new Vector(10,0);
+        this.add(knight);
+
+        const bullet = new Actor();
+        bullet.graphics.use(Resources.Bullet.toSprite());
+        bullet.pos = new Vector(75, 300);
+        bullet.vel = new Vector(30,0);
+        this.add(bullet);
+
     }
 }
 
--- src/js/resources.js
+++ src/js/resources.js
@@ -1,10 +1,18 @@
 import { ImageSource, Sound, Resource, Loader } from 'excalibur'
 
 const Resources = {
-    Fish: new ImageSource('images/fish.png')
+    Fish: new ImageSource('images/fish.png'),
+    Knight: new ImageSource('images/knight_left.png'),
+    Gun: new ImageSource('images/777_magnum.png'),
+    Bullet: new ImageSource('images/bullet.png'),
+    Background: new ImageSource('images/background.jpg')
 }
 const ResourceLoader = new Loader([
-    Resources.Fish
+    Resources.Fish,
+    Resources.Knight,
+    Resources.Gun,
+    Resources.Bullet,
+    Resources.Background
 ])
 
 export { Resources, ResourceLoader }
