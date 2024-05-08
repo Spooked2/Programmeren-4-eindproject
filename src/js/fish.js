bf5cd6f06d92c942628501ef9a061e2e8e60c20d
Sander <sanderlandm@gmail.com>
2024-05-08 12:29:10 +0200

    Added more fish and made them move

---
+++ src/js/fish.js
@@ -0,0 +1,18 @@
+import { Actor, Vector, Random } from "excalibur";
+import { Resources } from './resources.js';
+
+const random = new Random;
+
+export class Fish extends Actor {
+    constructor() {
+        super();
+
+        this.graphics.use(Resources.Fish.toSprite());
+        this.pos = new Vector(random.integer(1, 800), random.integer(1, 600));
+        this.vel = new Vector(random.integer(-20, 20), random.integer(-20, 20));
+
+    }
+
+
+
+}
--- src/js/game.js
+++ src/js/game.js
@@ -1,6 +1,7 @@
 import '../css/style.css'
-import { Actor, Engine, Vector } from "excalibur"
-import { Resources, ResourceLoader } from './resources.js'
+import { Actor, Engine, Vector } from "excalibur";
+import { Resources, ResourceLoader } from './resources.js';
+import { Fish } from "./fish.js";
 
 export class Game extends Engine {
 
@@ -11,23 +12,13 @@ export class Game extends Engine {
 
     startGame() {
         console.log("start de game!");
-        const fish = new Actor();
-        fish.graphics.use(Resources.Fish.toSprite());
-        fish.pos = new Vector(400, 300);
-        fish.vel = new Vector(-10,0);
-        this.add(fish);
 
-        const knight = new Actor();
-        knight.graphics.use(Resources.Knight.toSprite());
-        knight.pos = new Vector(50, 300);
-        knight.vel = new Vector(10,0);
-        this.add(knight);
+        for (let i = 0; i < 100; i++) {
+            const fish = new Fish();
+            this.add(fish);
+        }
 
-        const bullet = new Actor();
-        bullet.graphics.use(Resources.Bullet.toSprite());
-        bullet.pos = new Vector(75, 300);
-        bullet.vel = new Vector(30,0);
-        this.add(bullet);
+
 
     }
 }
