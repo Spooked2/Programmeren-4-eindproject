1f7068e75378a619bf398209d3023a0828f4dd2e
Sander <sanderlandm@gmail.com>
2024-05-14 16:09:34 +0200

    First attempt at collisions for fish

--- src/js/fish.js
+++ src/js/fish.js
@@ -1,17 +1,25 @@
-import { Actor, Vector, Random } from "excalibur";
+import {Actor, Vector, Random, CollisionType} from "excalibur";
 import { Resources } from './resources.js';
 
 const random = new Random;
 
 export class Fish extends Actor {
     constructor() {
-        super({width: Resources.Fish.width, height: Resources.Fish.height});
+        super({width: Resources.Fish.width, height: Resources.Fish.height, collisionType: CollisionType.Fixed});
 
         this.graphics.use(Resources.Fish.toSprite());
 
         this.scale = new Vector (0.4, 0.4);
 
-        this.on('pointerdown', () => {this.kill()})
+        this.on('pointerdown', () => {this.kill()});
+
+        this.on('collisionstart', () => {this.collisionHandler})
+
+    }
+
+    collisionHandler() {
+
+
 
     }
 
