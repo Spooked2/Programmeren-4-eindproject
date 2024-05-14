0fbb4aa3148dd0bc51f4a34cd270d1c0b9ca6330
Sander <sanderlandm@gmail.com>
2024-05-14 15:53:05 +0200

    Fish now have proper collision box

--- src/js/fish.js
+++ src/js/fish.js
@@ -4,14 +4,12 @@ import { Resources } from './resources.js';
 const random = new Random;
 
 export class Fish extends Actor {
-    constructor(position) {
-        super();
+    constructor() {
+        super({width: Resources.Fish.width, height: Resources.Fish.height});
 
         this.graphics.use(Resources.Fish.toSprite());
 
         this.scale = new Vector (0.4, 0.4);
-
-        this.pointer.useGraphicsBounds = true;
 
         this.on('pointerdown', () => {this.kill()})
 
